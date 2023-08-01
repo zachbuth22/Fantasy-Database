5657585a87a672c9e42b6d08641a42a3be38720f
Zach Buth <zachbuth@gmail.com>
2023-08-01 10:05:23 -0400

    2nd

--- Top 100 OOP/RB.cs
+++ Top 100 OOP/RB.cs
@@ -3,7 +3,16 @@ namespace Top_100_OOP
 {
 	public class RB
 	{
-		
-	}
+		//prop
+		public int RushYards { get; set; }
+		public int Carries { get; set; }
+		public int Healthy { get; set; }
+		//constructors
+		public RB(string _name, int _recYards, int _targets, int _receptions, int _tDs,
+			int _games, int _rushYards, int _carries, int _healthy)
+			: base(_name, _recYards, _targets, _receptions, _tDs);
+			
+
+    }
 }
 
